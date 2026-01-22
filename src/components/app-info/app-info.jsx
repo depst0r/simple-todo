0c5aebfd85cf9c349cf4e966ddb96fdc303ce005
Sergey Panteleenko <depstor90@gmail.com>
2026-01-22 23:43:06 +0300

    searc-panel

---
+++ src/components/app-info/app-info.jsx
@@ -0,0 +1,12 @@
+import './app-info.css';
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников в компании Printer-Hub</h1>
+            <h2>Оьщее число сотрудников:</h2>
+            <h2>Премию получат:</h2>
+        </div>
+    )
+}
+
+export default AppInfo;
