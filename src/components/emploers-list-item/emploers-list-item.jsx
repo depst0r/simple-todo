be07db503e0b1d0878ea3f75547a989f4f338315
Sergey Panteleenko <depstor90@gmail.com>
2026-01-28 23:57:42 +0300

    28

--- src/components/emploers-list-item/emploers-list-item.jsx
+++ src/components/emploers-list-item/emploers-list-item.jsx
@@ -1,6 +1,22 @@
-import './employees-list-item.css';
-const EmployeesListItem = ({name, salary, increase}) => {
+import { Component } from 'react';
 
+import './employees-list-item.css';
+class EmployeesListItem extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false
+        }
+    }
+
+    onIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }))
+    }
+
+render() {
+    const {name, salary, increase} = this.props;
     let classNames = 'list-group-item d-flex justify-content-between';
 
     // if (increase) {
@@ -15,7 +31,9 @@ const EmployeesListItem = ({name, salary, increase}) => {
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
-                    className="btn-cookie btn-sm ">
+                    className="btn-cookie btn-sm "
+                    onClick={this.onIncrease}
+                    >
                     <i className="fas fa-cookie"></i>
                 </button>
 
@@ -27,6 +45,7 @@ const EmployeesListItem = ({name, salary, increase}) => {
             </div>
         </li>
     )
+}
 }
 
 export default EmployeesListItem;
