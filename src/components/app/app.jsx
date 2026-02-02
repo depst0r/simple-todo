cc18e3e081e341bdf6fe8ade3f2fd8cba406a286
Sergey Panteleenko <depstor90@gmail.com>
2026-02-02 23:26:54 +0300

    term

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -16,7 +16,8 @@ class App extends Component{
                     {name: 'Johm C.' , salary: '800', increase: false, rise: true, id: 1,},
                     {name: 'Carl W.' , salary: '3000', increase: true, rise: false, id: 2,},
                     {name: 'Alex M.' , salary: '5000', increase: false, rise: false, id: 3,},
-                ]
+                ],
+                term: ''
         }
         this.maxId = 4;
     }
@@ -59,31 +60,34 @@ class App extends Component{
         }))
     }
 
-    // onToggleRise = id => {
-    //             this.setState(({data}) => ({
-    //         data: data.map(item => {
-    //             if (item.id === id) {
-    //                 return {
-    //                     ...item, rise: !item.rise
-    //                 }
-    //             }
-    //             return item
-    //         })
-    //     }))
-    // }
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = term => {
+        this.state ({term})
+    }
 
    render () {
+    const {data, term} = this.state
     const employes = this.state.data.length;
     const increased = this.state.data.filter(item => item.increase).length;
+    const visibleData = this.searchEmp(data, term)
      return (
         <div className="app">
             <AppInfo employes={employes} increased={increased}/>
             <div className="search-panel">
-                <SearchPanel/>
+                <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
                 <AppFilter/>
             </div>
             <EmploersList 
-            data={this.state.data}
+            data={visibleData}
             onDelete={id => this.deleteItem(id)}
             onToggleProp={this.onToggleProp}
             />
--- src/components/search-panel/search-panel.jsx
+++ src/components/search-panel/search-panel.jsx
@@ -1,12 +1,23 @@
+
+import { Component } from 'react';
 import './search-panel.css'
 
-const SearchPanel = () => {
+class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+    }
+render() {
     return (
         <input 
         type="text" 
         className="form-control search-input" 
         placeholder="Найти сотрудника"/>
     )
+}
+
 }
 
 export default SearchPanel;
